9302e3f73be64347a144aa8c25759cb22b8a93fe
Lucas Atayde <lsa4@rice.edu>
2018-12-01 14:17:21 -0600

    Text Messaging Initial

--- SMS/run.py
+++ SMS/run.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+from twilio.rest import Client
 app = Flask(__name__)
 
-@app.route("/sms")
-def hello():
-    return "Hello World!"
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_ahoy_reply():
+    """Respond to incoming messages with a friendly SMS."""
+    # Start our response
+    resp = MessagingResponse()
+    message = request.args.get('Body')
+    print(message)
+    # Add a message
+    resp.message("Ahoy! Thanks so much for your message.")
+
+    return str(resp)
 
 if __name__ == "__main__":
     app.run(debug=True)
